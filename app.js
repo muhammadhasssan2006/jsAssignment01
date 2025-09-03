8b6555909ff6c51ada8cde31b0ed7926fc486232
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-09-03 15:54:14 +0500

    js assignment 01

---
+++ app.js
@@ -0,0 +1,101 @@
+// Assignment 01 
+//-------------------- Chapter 01----------------------- 
+
+// Alert
+
+// Questions:-
+
+// 01)
+// alert("Hello Well come to my website")
+
+// 02)
+// alert("Error! Please enter a valid password")
+
+// 03)
+// alert("Welcome to js land... \n Happy coding! ")
+
+// 04)
+// alert("Welcome to js land...")
+// alert("Happy coding!")
+
+// 05)
+// alert("Hello... I can run js through my web browser's console ")
+
+
+// -----------------------chapter 02-------------------
+
+// Questions:-
+
+// 01)
+// var Username;
+// console.log(Username);
+
+// 02)
+// var myName = "Muhammad Hassan";
+// alert(myName);
+
+// 03)
+// var message = "Hello World!";
+// alert(message);
+
+// 04)
+// var myName = "Muhammad Hassan";
+// alert(myName);
+
+// var myAge = 18;
+// alert(myAge);
+
+// var bio = "Certified Mobile Application Development";
+// alert(bio);
+
+
+// 05)
+// var food = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert(food);
+
+// 06)
+// var email = "MY email address is hassanweb365@gmil.com";
+// alert(email);
+
+
+// 07)
+// var book = "I am trying to learn from the book a smarter way to learn javaScript";
+// alert(book);
+
+
+// 08)
+// var msg = "Yah! I can Write HTML content throught javascript";
+// document.write(msg);
+
+
+// 09)
+// var speialCharacter = "-----------$$$$$--------------";
+// alert(speialCharacter);
+
+
+// --------------------Chapter 03--------------------
+
+// Questions:-
+
+// 01)
+// var age = "I am 15 yesr old";
+// alert(age);
+
+
+// 02)
+// var reminder = "You have visited this site 14 times";
+// alert(reminder);
+
+
+// 03)
+// var birthYear = "My birth year is 2006 <br> Data type of my decleared variavle is number";
+// document.write(birthYear);
+
+// 04)
+// var visitorName = "<b> Muhammad Hassan </b>";
+// var productTittle = "ordered";
+// var quanitty = "<b> 5 T-shirts</b>(m)";
+// var storeName = "ON XYZ Clothing store.";
+// document.write(visitorName+productTittle+quanitty+storeName);
+
+
