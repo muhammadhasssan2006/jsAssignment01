ca9eddb07292c52651ad92379ef22e3487a4c75e
muhammadhasssan2006 <hassanweb365@gmail.com>
2025-09-05 16:03:13 +0500

    assignment complete

--- app.js
+++ app.js
@@ -98,4 +98,352 @@
 // var storeName = "ON XYZ Clothing store.";
 // document.write(visitorName+productTittle+quanitty+storeName);
 
+// -----------------Chapter 04-----------------------------
+
+// Questions:-
+
+// 01)
+// var Firstname = "Muhammad";var Middlename = "Hassan";var Lastname = "Khan";
+// document.write(Firstname+" "+Middlename+" "+Lastname);
+
+// 02)
+// legalVariables-------
+// var legalVariables = "<b> 5 Legal Names for Variables are:</b> <br>";
+// var legalVar1 = "01) firstName <br>";
+// var legalVar2 = "02) $firstName <br>";
+// var legalVar3 = "03) _firstName <br>";
+// var legalVar4 = "04) first_Name <br>";
+// var legalVar5 = "05) firstName01 <br>";
+
+// document.write(legalVariables,legalVar1,legalVar2,legalVar3,legalVar4,legalVar5);
+
+// illegal variables--------
+// var illegalVariables = "<b> 5 ilLegal Names for Variables are:</b> <br>";
+// var illegalVar1 = "01) function (js keyword can't be used as variable name) <br>";
+// var illegalVar2 = "02) *firstName <br>";
+// var illegalVar3 = "03) first-Name <br>";
+// var illegalVar4 = "04) first Name <br>";
+// var illegalVar5 = "05) 01firstName <br>";
+
+// document.write(illegalVariables,illegalVar1,illegalVar2,illegalVar3,illegalVar4,illegalVar5);
+
+// 03)
+
+// var heading = "<h1>Rules for writting js variables </h1><br>";
+
+// var varRules = "Variable names can only contain, numbers,$and _. for example: $my_1stVariable.<br> Variable must begin with a letter,$ or _. for example: $namae,_name or name. <br> variable names are case sensitive. <br> Variable names should not be JS keywords. ";
+
+// document.write(heading,varRules);
+
+// ----------------------------------chapter 05-------------------------
+// Questions:--
+
+// 01)-------------------------------------------------
+// var input1 = +prompt("Sum of  ");
+// var input2 = +prompt("and  ");
+// var result = input1 + input2;
+// console.log( "sum is = " + result);
+// 02)--------------------------------------------
+// var input3 = +prompt("Subtraction of  ");
+// var input4 = +prompt("and  ");
+// var result = input3 - input4;
+// console.log( "subtraction is = " + result);
+
+
+// var input5 = +prompt("multiple of  ");
+// var input6 = +prompt("and  ");
+// var result = input5 * input6;
+// console.log( "multiple is = " + result);
+
+
+// var input7 = +prompt("divide of  ");
+// var input8 = +prompt("and  ");
+// var result = input7 / input8;
+// console.log( "divide is = " + result);
+
+
+// 03)----------------------------------------
+// var ha$$an ;
+// document.write( "Vale of variable declaration is " +ha$$an +"<br>");
+
+// var initialValue = 5;
+// document.write("initial value is " + initialValue + "<br>" );
+
+// var increasedValue = ++initialValue;
+// document.write(" value after increment is  " + increasedValue + "<br>" );
+
+// var additioon = increasedValue + 7;
+// document.write(" value after Addition is  " + additioon + "<br>" );
+
+// var decreasedValue = --additioon;
+// document.write(" value after decrement is  " + decreasedValue + "<br>" );
+
+// var reminder = decreasedValue % 3;
+// document.write(" reminder is   " + reminder  );
+
+
+// 04)---------------------------------------
+// var ticketCost = 600;
+// var costOf_5 = ticketCost * 5  ;
+// document.write("Total cost of buying 5 tickets to a movie is " + costOf_5 + "PKR")
+
+// 05)-----------------------------------
+// var tbl = "<h2> Table of 4 </h2> <br>";
+// var value1 = 4*1;
+// var value2 = 4*2;
+// var value3 = 4*3;
+// var value4 = 4*4;
+// var value5 = 4*5;
+// var value6 = 4*6;
+// var value7 = 4*7;
+// var value8 = 4*8;
+// var value9 = 4*9;
+// var value10 = 4*10;
+
+// document.write(tbl);
+// document.write("4x1=" + value1 + "<br>")
+// document.write("4x2=" + value2 + "<br>")
+// document.write("4x3=" + value3 + "<br>")
+// document.write("4x4=" + value4 + "<br>")
+// document.write("4x5=" + value5 + "<br>")
+// document.write("4x6=" + value6 + "<br>")
+// document.write("4x7=" + value7 + "<br>")
+// document.write("4x9=" + value9 + "<br>")
+// document.write("4x10=" + value10 + "<br>")
+
+// 06)--------------------------------------
+
+// var celscius = +prompt("Enter celscius Temperature");
+// var fahrenheit = (celscius *9/5)+32;
+
+// document.write("Temperature in Fahrenheit is "+ fahrenheit);
+
+
+// var Fahrenheit = +prompt("Enter Fahrenheit Temperature");
+// var Celscius = (Fahrenheit - 32) * 5/9;
+
+// document.write("Temperature in Celsicus is "+ Celscius);
+
+
+// 07)------------------------------------------
+// var item1 ="Price of item 1 is 650 <br>"
+// var price1 = 650
+// var quantity1= prompt("Quantity of item1 is " )
+// var item2 ="Price of item 2 is 100 <br>"
+// var price2 = 100;
+// var quantity2= prompt("Quantity of item2 is  ")
+// var shipingCharges = "Shipping Charges 100 "
+// var shipCgarge = 100;
+// var total = price1*quantity1+price2*quantity2+shipCgarge 
+
+
+// document.write(item1);
+// document.write("Quantity of item 1 is " + quantity1 + "<br>");
+
+// document.write(item2);
+// document.write("Quantity of item 2 is " + quantity2 +"<br>");
+// document.write(shipingCharges);
+
+// document.write("<br> The Total of your order is "+ total);
+
+
+// 08)----------------------------------------------
+
+// var markSheet = "<h1>MarkSheet</h1> <br>";
+// var marksObtained= prompt("Enter Marks Obtained by Student");
+// var totalMarks = prompt("Enter Total Marks ");
+
+// var Percentage = marksObtained*100/totalMarks;
+
+// document.write(markSheet,"Obtained marks "+marksObtained,"<br> Total Marks ",totalMarks);
+// document.write("<br> Precentage is " +Percentage);
+
+
+// 09)-------------------------------------------
+
+// var heaDing = "<h1>Currency in PKR </h1>";
+// var CurrencyExchange = 10*283.99 + 25 *75.09 ;
+
+// document.write("Total currency in PKR:"+ heaDing +"<br>" +CurrencyExchange);
+
+// 10)------------------------------------------
+
+// var a = 3;
+// var calculation = a + 5 * 10 / 2;
+// document.write("The calculation of whole expression is "+calculation);
+
+
+// 11)--------------------------------------------
+// var calculator = "<h1>Age Calculator</h1>";
+// var currentYear = prompt("Current Year:");
+// var birthYear = prompt("Enrter Your BirthYear:");
+
+
+// var age = currentYear-birthYear;
+
+// document.write(calculator);
+// document.write(" Current Year: "+currentYear);
+// document.write("<br> Birth Year: "+birthYear);
+// document.write("<br> Your Current Age is: "+age);
+
+// 12)-----------------------------------------------
+// var geomatery = "<h1>The Geometrizer:</h1>";
+// var radius = prompt("Enter radius of Circle");
+// var circumference = 2*3.14*radius;
+// var area = 3.14*radius*radius;
+
+// document.write(geomatery);
+// document.write("<br> circumference of Cirle is " + circumference);
+// document.write("<br> radius of Cirle is " + radius);
+// document.write("<br> Area of circle is " +area);
+
+// 13)---------------------------------------------------
+
+// var LTSCalculator = "<h1>The LifeTime Supply Calculator</h1>";
+// var snack =prompt("Favourite Snack:");
+// var crtAge = prompt("Enter Your Current Age");
+// var maxAgr = prompt("Expected Maximum Age:");
+// var pdSnack = prompt("Number of Snacks per day:")
+// var ageLeft =maxAgr-crtAge;
+// var snackPY = pdSnack*365;
+// var LifeTimeSnack = snackPY*ageLeft; 
+
+// document.write(LTSCalculator);
+// document.write("<br> Favourite Snack:"+snack);
+// document.write("<br> Current Age:"+crtAge);
+// document.write("<br> Expected Maximum Age:"+maxAgr);
+// document.write("<br> Number of Snacks per day:"+pdSnack);
+// document.write("<br> You will need " + LifeTimeSnack + " "+ snack + " to last you until the ripe old age of " + maxAgr  );
+
+
+
+
+// ------------------------------------------Chapte 6--9------------------------------------------------------
+// Questions:-----
+
+// 01)---------------------------------------------
+// var result = "<h2>Results:</h2>";
+// var a = 10;
+// var b = ++a;
+
+// document.write(result);
+// document.write("<br> The value of a is : 10" );
+// document.write("<br> ------------------------------------");
+// document.write("<br> The value of  is : "+b);
+// document.write("<br> Now the vaue of ++a is: " +b);
+
+// document.write("<br><br> The vaue of a++ is: ",b++);
+// document.write("<br>     The vaue of a is: ",b);
+
+// document.write("<br><br> Now the vaue of --a is: " ,--b);
+// document.write("<br>  The vaue of a is: " ,b);
+
+// document.write("<br><br>  The vaue of a-- is: " ,b--);
+// document.write("<br>  The vaue of a is: " ,b);
+
+// 02)------------------------------------------------------
+
+// var x = 2;
+// var y =1;
+
+// var result = --x - --y + ++y + y-- ;
+// document.write("a is: ",--x);
+// document.write("<br> b is: ",--x - --y);
+// document.write("<br> c is: " , --x - --y + ++y);
+// document.write("<br> d is: ",--x - --y + ++y + y--);
+// document.write("<br> result is: ",result);
+
+// 03)------------------------------------------
+
+// var enterName = prompt("Enter your Name");
+// document.write("Hello "+enterName+"! Welcome to my website.");
+
+// 04)-----------------------------------------------
+
+// var enterNumber =prompt("Enter any Number");
+
+// var value1 = enterNumber*1;
+// var value2 = enterNumber*2;
+// var value3 = enterNumber*3;
+// var value4 = enterNumber*4;
+// var value5 = enterNumber*5;
+// var value6 = enterNumber*6;
+// var value7 = enterNumber*7;
+// var value8 = enterNumber*8;
+// var value9 = enterNumber*9;
+// var value10 = enterNumber*10;
+
+// document.write("The Table of "+enterNumber ,"<br><br>");
+// document.write(enterNumber+"x1=" + value1 + "<br>")
+// document.write(enterNumber+"x2=" + value2 + "<br>")
+// document.write(enterNumber+"x3=" + value3 + "<br>")
+// document.write(enterNumber+"x4=" + value4 + "<br>")
+// document.write(enterNumber+"x5=" + value5 + "<br>")
+// document.write(enterNumber+"x6=" + value6 + "<br>")
+// document.write(enterNumber+"x7=" + value7 + "<br>")
+// document.write(enterNumber+"x9=" + value9 + "<br>")
+// document.write(enterNumber+"x10=" + value10 + "<br>")
+
+// 06)--------------------------------------------------
+
+var subject1=prompt("Enter Subject1 Name");
+var subject2=prompt("Enter Subject2 Name");
+var subject3=prompt("Enter Subject3 Name");
+var ts = "Total Marks "
+var marks1 =+prompt("Enter Subject1 Obtained Marks");
+var marks2 =+prompt("Enter Subject2 Obtained Marks");
+var marks3 =+prompt("Enter Subject3 Obtained Marks");
+
+var tMarks = 100;
+var totalMarks = tMarks*3;
+var totalobtMarks = marks1+marks2+marks3;
+
+var percent1 = marks1*100/tMarks;
+var percent2 = marks2*100/tMarks;
+var percent3 = marks3*100/tMarks;
+
+var percentAge = totalobtMarks*100/totalMarks;
+
+document.write("<table border='1' cellspacing='0' cellpadding='5'> " );
+
+document.write("<tr><th>Subjects</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage%</th></tr>");
+
+document.write("<tr><td>"+subject1+"</td>"+"<td>"+tMarks+"</td>"+"<td>"+marks1+"</td>"+"<td>"+percent1+"%"+"</td></tr>");
+document.write("<tr><td>"+subject2+"</td>"+"<td>"+tMarks+"</td>"+"<td>"+marks2+"</td>"+"<td>"+percent2+"%"+"</td></tr>");
+document.write("<tr><td>"+subject3+"</td>"+"<td>"+tMarks+"</td>"+"<td>"+marks3+"</td>"+"<td>"+percent3+"%"+"</td></tr>");
+
+
+
+
+document.write("<tr>"+"<td>"+ts+"</td>"+"<td>"+totalMarks+"</td>"+"<td>"+totalobtMarks+"</td>"+"<td>"+percentAge+"%"+"</td></tr></table>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
